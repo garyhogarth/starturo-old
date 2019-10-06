e16fae0cd9432b7fb842a5e1c21feca3082bcf6d
Gary Hogarth <gary.hogarth@tomtom.com>
2019-10-06 12:56:46 +0100

    Update libararies

--- src/examples/Examples.tsx
+++ src/examples/Examples.tsx
@@ -1,15 +1,100 @@
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
-import { Button, Card, Text } from 'react-native-elements'
+import React from 'react'
+import { Linking, ScrollView } from 'react-native'
+import { ListItem } from 'react-native-elements'
 import { useNavigation } from 'react-navigation-hooks'
 
 const TAG = 'Examples'
 
+const links = [
+  {
+    name: 'Gary Hogarth',
+    icon: 'linkedin',
+    url: 'http://linkedin.com/in/garyhogarth',
+  },
+  {
+    name: '@garyhogarth',
+    icon: 'twitter',
+    url: 'http://twitter.com/garyhogarth',
+  },
+  {
+    name: 'http://github.com/garyhogarth',
+    icon: 'github',
+    url: 'http://github.com/garyhogarth',
+  },
+]
+
 export const Examples = () => {
   const { navigate } = useNavigation()
   return (
-    <>
-      <Button onPress={() => navigate('Counter')} testID={`${TAG} Button Counter`} title='Counter' />
-    </>
+    <ScrollView>
+      <ListItem
+        testID={`${TAG} Button Counter`}
+        title='Counters'
+        subtitle='Counters using all including state management tools'
+        onPress={() => navigate('Counter')}
+        chevron
+        bottomDivider
+      />
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+      {links.map((link, i) => (
+        <ListItem
+          key={i}
+          testID={`${TAG} Contact ${link.name}`}
+          leftIcon={{ name: link.icon, type: 'font-awesome' }}
+          title={link.name}
+          onPress={() => Linking.openURL(link.url)}
+          chevron
+        />
+      ))}
+    </ScrollView>
   )
 }
